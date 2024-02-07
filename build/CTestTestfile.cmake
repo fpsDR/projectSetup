6adcba6d2092691d84b47c4ecd88214bc060de54
fpsDR <fps@datarespons.no>
2024-02-07 15:34:21 +0100

    removing build directory in googleTest.yml

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/fpsdr/projects/projectSetup
+# Build directory: /home/fpsdr/projects/projectSetup/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/fpsdr/projects/projectSetup/build/hello_test[1]_include.cmake")
+subdirs("_deps/googletest-build")
